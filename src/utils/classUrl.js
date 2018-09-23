a449bd68d4d21fdf5fa393799d6186c1ca8c953e
Shubham <shubham@castiko.com>
2018-09-23 16:25:32 +0530

    added page count

--- src/utils/classUrl.js
+++ src/utils/classUrl.js
@@ -4,11 +4,10 @@ export default class URL{
     constructor(page){
         this.page = page
     }
+    //Fetching list of page according to page cound
     fetchAllPostURL(){
-        return `https://content.guardianapis.com/search?api-key=${secret_key}&show-fields=thumbnail,bodyText&page=${this.page}`
+        return `https://content.guardianapis.com/search?api-key=${secret_key}&show-fields=thumbnail,headline,byline,bodyText&page=${this.page}`
     }
-    fetchSpecificPostURL(){
-        return `https://content.guardianapis.com/${this.id}?api-key=${secret_key}&show-fields=thumbnail,headline,byline,bodyText`
-    }
+    
     
 }
